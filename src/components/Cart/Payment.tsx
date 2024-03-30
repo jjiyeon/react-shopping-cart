5970c83c86f3e7e7b5d244e7c8abed427c74b9cc
jiyeon <ejy1105@gmail.com>
2024-03-30 13:52:48 +0900

    feat: 장바구니에서 주문 결제로 넘어가는 클릭 이벤트 추가

--- src/components/Cart/Payment.tsx
+++ src/components/Cart/Payment.tsx
@@ -1,11 +1,27 @@
 import { useContext } from 'react'
-import { CartsContext } from '../../context/cartsContext'
+import { CartContext, UpdateCartContext } from '../../context/cartsContext'
+import { useNavigate } from '@tanstack/react-router'
+import { fomattingComma } from '../../util/formatter'
 
 const Payment = () => {
-  const myCarts = useContext(CartsContext)
+  const myCart = useContext(CartContext)
+  const updateCart = useContext(UpdateCartContext)
 
-  const itemAmount = myCarts.cart.filter((item) => item.isChecked)
+  const itemAmount = myCart.cart.filter((item) => item.isChecked)
   const estimatedPrice = itemAmount.reduce((acc, curr) => (acc += curr.price * curr.quantity), 0)
+  const navigate = useNavigate()
+
+  const handleOrderClick = () => {
+    if (window.confirm('주문목록으로 이동할게요!')) {
+      navigate({ to: '/order' })
+      //장바구니에서 체크된 상품들이 삭제된다
+      updateCart({
+        ...myCart,
+        order: myCart.cart.filter((item) => item.isChecked),
+        cart: myCart.cart.filter((item) => !item.isChecked),
+      })
+    }
+  }
 
   return (
     <section className="cart-right-section">
@@ -16,14 +32,10 @@ const Payment = () => {
       <div className="cart-right-section__bottom">
         <div className="flex justify-between p-20 mt-20">
           <span className="highlight-text">결제예상금액</span>
-          <span className="highlight-text">{estimatedPrice.toLocaleString('ko-KR')}원</span>
+          <span className="highlight-text">{fomattingComma(estimatedPrice)}원</span>
         </div>
         <div className="flex-center mt-30 mx-10">
-          <button
-            className="primary-button flex-center"
-            disabled={itemAmount.length === 0}
-            onClick={() => console.log('주문 클릭')}
-          >
+          <button className="primary-button flex-center" disabled={itemAmount.length === 0} onClick={handleOrderClick}>
             주문하기{`(${itemAmount.length})`}
           </button>
         </div>
