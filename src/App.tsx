1319acf6bb827d99cbb93f520b609cc6eaca8538
jiyeon <ejy1105@gmail.com>
2024-04-09 01:42:29 +0900

    temp: msw handler 주석

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const router = createRouter({
     myOrder: {
       cart: [],
       order: [],
+      orderHistory: [],
     },
   },
 })
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,11 +4,11 @@ import list from './db.json'
 export const handlers = [
   // http.get('/products', () => HttpResponse.json(list.products)),
   http.get('/orders', () => HttpResponse.json(list.orders)),
-  http.get('/products/:id', ({ params }) => {
-    const { id } = params
-    const filtered = list.products.filter((item, _) => item.id === Number(id))
+  // http.get('/products/:id', ({ params }) => {
+  //   const { id } = params
+  //   const filtered = list.products.filter((item, _) => item.id === Number(id))
 
-    return HttpResponse.json(filtered[0])
-  }),
+  //   return HttpResponse.json(filtered[0])
+  // }),
   http.get('/carts', () => HttpResponse.json(list.carts)),
 ]
