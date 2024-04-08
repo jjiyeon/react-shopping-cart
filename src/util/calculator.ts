772c29b884dc005777cab5e0455f9887759b2303
jiyeon <ejy1105@gmail.com>
2024-04-08 23:01:41 +0900

    fix: 함수 naming 바꿔보기 estimatedPrice -> sumPrice

--- src/util/calculator.ts
+++ src/util/calculator.ts
@@ -1,8 +1,8 @@
 import { CartItem } from '../context/cartsContext'
 
-export const estimatedPrice = (param: { item: Array<CartItem> }) => {
+export const sumPrice = (param: { item: Array<CartItem> }) => {
   return param.item.reduce((acc, curr) => (acc += curr.price * curr.quantity), 0)
 }
-export const CheckeditemCount = (param: { item: Array<CartItem> }) => {
+export const checkedItemCount = (param: { item: Array<CartItem> }) => {
   return param.item.filter((item) => item.isChecked)
 }
